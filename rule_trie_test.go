f51c9e49ab4a865663bbedba6f0cff0dddd0eac8
Denis Germano <dlcgermano@gmail.com>
2020-03-24 21:07:25 -0300

    Cleaning

--- rule_trie_test.go
+++ rule_trie_test.go
@@ -2,8 +2,6 @@ package adblockgoparser
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestTrie(t *testing.T) {
@@ -18,16 +16,4 @@ func TestTrie(t *testing.T) {
 	// fmt.Println(root)
 	// fmt.Println(child)
 	// fmt.Println(child.parent)
-
-	rules := []string{"/banner/*/img$domain=example.com|~bar.example.com"}
-	ruleSet, err := NewRuleSetFromStr(rules)
-	assert.NoError(t, err)
-	// Block for `example.com` domain and its subdomain
-	assert.False(t, ruleSet.Allow(reqFromURL("http://example.com/banner/foo/img")))
-	assert.False(t, ruleSet.Allow(reqFromURL("http://anysubdomain.example.com/banner/foo/img")))
-	// // But not for `bar` subdomain and its subdomain or other domain
-	assert.True(t, ruleSet.Allow(reqFromURL("http://bar.example.com/banner/foo/img")))
-	assert.True(t, ruleSet.Allow(reqFromURL("http://foo.bar.example.com/banner/foo/img")))
-	assert.True(t, ruleSet.Allow(reqFromURL("http://example.net/banner/foo/img")))
-	assert.True(t, ruleSet.Allow(reqFromURL("http://anysubdomain.example.net/banner/foo/img")))
 }
