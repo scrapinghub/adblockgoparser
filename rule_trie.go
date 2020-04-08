f96d9707d8dad3ed930cf667ea00a17fe0a50233
Denis Germano <dlcgermano@gmail.com>
2020-04-08 18:52:35 -0300

    Clean code

--- rule_trie.go
+++ rule_trie.go
@@ -62,60 +62,9 @@ func (matcher *Matcher) Match(req *Request) bool {
 	}
 
 	// Match domain and subdomains
-	hostname := strings.ToLower(req.URL.Hostname())
 	for _, rule := range matcher.domainNameRules {
-		allowed := true
-		if len(rule.domains) > 0 {
-			for domain, active := range rule.domains {
-				if !(strings.Contains(hostname, domain) == active) {
-					allowed = false
-					break
-				}
-			}
-			if allowed && strings.HasSuffix(hostname, rule.ruleText[2:len(rule.ruleText)-1]) {
-				matchDomain := true
-				matchOption := true
-				if len(rule.options) > 0 {
-					matchOption = false
-					for option, active := range rule.options {
-						switch {
-						case option == "xmlhttprequest":
-						case option == "third-party":
-						case option == "script":
-							switch filepath.Ext(path) {
-							case ".js":
-								matchOption = active
-							default:
-								matchOption = !active
-							}
-						case option == "image":
-							switch filepath.Ext(path) {
-							case ".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".jpeg2000":
-								matchOption = active
-							default:
-								matchOption = !active
-							}
-						case option == "stylesheet":
-							switch filepath.Ext(path) {
-							case ".css":
-								matchOption = active
-							default:
-								matchOption = !active
-							}
-						case option == "font":
-							switch filepath.Ext(path) {
-							case ".otf", ".ttf", ".fnt":
-								matchOption = active
-							default:
-								matchOption = !active
-							}
-						}
-					}
-				}
-				if matchDomain && matchOption {
-					return true
-				}
-			}
+		if matchDomains(rule, req) && matchOptions(rule, req) {
+			return true
 		}
 	}
 
@@ -133,65 +82,9 @@ func (matcher *Matcher) Match(req *Request) bool {
 func (pathMatcher *PathMatcher) findNext(runes []rune, req *Request) bool {
 	match := false
 	if len(pathMatcher.rules) != 0 {
-		path := strings.ToLower(req.URL.Path)
-		if strings.HasSuffix(path, ".gz") {
-			path = path[:len(path)-len(".gz")]
-		}
 		for _, rule := range pathMatcher.rules {
-			allowed := true
-			if len(rule.domains) > 0 {
-				hostname := strings.ToLower(req.URL.Hostname())
-				for domain, active := range rule.domains {
-					if !(strings.Contains(hostname, domain) == active) {
-						allowed = false
-						break
-					}
-				}
-			}
-
-			if allowed && rule.regex.MatchString(req.URL.String()) { // This line need to be removed and add simpler validation
-				match = true
-				matchOption := true
-				if len(rule.options) > 0 {
-					matchOption = false
-					for option, active := range rule.options {
-						switch {
-						case option == "xmlhttprequest":
-						case option == "third-party":
-						case option == "script":
-							switch filepath.Ext(path) {
-							case ".js":
-								matchOption = matchOption || active
-							default:
-								matchOption = matchOption || !active
-							}
-						case option == "image":
-							switch filepath.Ext(path) {
-							case ".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".jpeg2000":
-								matchOption = matchOption || active
-							default:
-								matchOption = matchOption || !active
-							}
-						case option == "stylesheet":
-							switch filepath.Ext(path) {
-							case ".css":
-								matchOption = matchOption || active
-							default:
-								matchOption = matchOption || !active
-							}
-						case option == "font":
-							switch filepath.Ext(path) {
-							case ".otf", ".ttf", ".fnt":
-								matchOption = matchOption || active
-							default:
-								matchOption = matchOption || !active
-							}
-						}
-					}
-				}
-				if match && matchOption {
-					return true
-				}
+			if matchDomains(rule, req) && matchOptions(rule, req) && rule.regex.MatchString(req.URL.String()) { // This line need to be removed and add simpler validation
+				return true
 			}
 		}
 	}
@@ -216,4 +109,63 @@ func (pathMatcher *PathMatcher) findNext(runes []rune, req *Request) bool {
 	}
 
 	return false
+}
+
+func matchDomains(rule *ruleAdBlock, req *Request) bool {
+	allowedDomain := true
+	if len(rule.domains) > 0 {
+		hostname := strings.ToLower(req.URL.Hostname())
+		for domain, active := range rule.domains {
+			if !(strings.HasSuffix(hostname, domain) == active) {
+				allowedDomain = false
+				break
+			}
+		}
+	}
+	return allowedDomain
+}
+func matchOptions(rule *ruleAdBlock, req *Request) bool {
+	matchOption := true
+	path := strings.ToLower(req.URL.Path)
+	if strings.HasSuffix(path, ".gz") {
+		path = path[:len(path)-len(".gz")]
+	}
+	if len(rule.options) > 0 {
+		matchOption = false
+		for option, active := range rule.options {
+			switch {
+			case option == "xmlhttprequest":
+			case option == "third-party":
+			case option == "script":
+				switch filepath.Ext(path) {
+				case ".js":
+					matchOption = matchOption || active
+				default:
+					matchOption = matchOption || !active
+				}
+			case option == "image":
+				switch filepath.Ext(path) {
+				case ".jpg", ".jpeg", ".png", ".gif", ".webp", ".tiff", ".psd", ".raw", ".bmp", ".heif", ".indd", ".jpeg2000":
+					matchOption = matchOption || active
+				default:
+					matchOption = matchOption || !active
+				}
+			case option == "stylesheet":
+				switch filepath.Ext(path) {
+				case ".css":
+					matchOption = matchOption || active
+				default:
+					matchOption = matchOption || !active
+				}
+			case option == "font":
+				switch filepath.Ext(path) {
+				case ".otf", ".ttf", ".fnt":
+					matchOption = matchOption || active
+				default:
+					matchOption = matchOption || !active
+				}
+			}
+		}
+	}
+	return matchOption
 }
